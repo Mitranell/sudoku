fe2fadcebcfa2924e988aad1c8b09e8ee24b6fd6
Mitranell <milantheeuwes@live.nl>
2018-05-17 18:37:11 +0100

    hi

--- sudoku.c
+++ sudoku.c
@@ -80,7 +80,8 @@ int main(int argc, char *argv[]) {
     }
 
     // nasty calculation for number of nodes
-    int nodes[(int)((n - thread_rank - 1) / nprocs) + 1];
+    int *nodes;
+    nodes = (int*)malloc(((int)((n - thread_rank - 1) / nprocs) + 1) * sizeof(int));
     /* fill nodes with nodes to check
      * example thread 2 and 5 processors: 2, 7, 12, 17, ...
      */
@@ -102,7 +103,7 @@ int main(int argc, char *argv[]) {
             updateCell(backtrackCell.i, backtrackCell.j, i);
 
             printf("Thread %d with last element %d\n", thread_rank, nodes[sizeof(nodes)/sizeof(nodes[0]) - 1]);
-            nodes = (int *) realloc(nodes, sizeof(nodes) - sizeof(nodes)/sizeof(nodes[0]));
+            nodes = (int *)realloc(nodes, sizeof(nodes) - sizeof(nodes)/sizeof(nodes[0]));
 
             if (solve(nodes)) {
                 solved = 1;
