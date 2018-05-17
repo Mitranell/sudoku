ff09a078b9c45f19c7b5f12a8adc2d3b07a70cfb
Mitranell <milantheeuwes@live.nl>
2018-05-17 18:26:00 +0100

    hi

--- main.c
+++ main.c
@@ -103,7 +103,6 @@ int solve(int *nodes) {
 
     // the sudoku is not solvable, so trigger backtracking
     if (rating == 0) {
-        printf("Thread %d with array size %d", thread_rank, sizeof(nodes)/sizeof(nodes[0]));
         return 0;
     }
 
