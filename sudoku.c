cff055bab6400c265bbd742552b59596e6c7025b
Ali <buyukkakac@gmail.com>
2018-05-17 18:32:33 +0100

    hi

--- sudoku.c
+++ sudoku.c
@@ -95,7 +95,10 @@ int main(int argc, char *argv[]) {
                 solved = 1;
                 possible_root = thread_rank;
                 printf("solved: before all reduce, %d\n", thread_rank);
-                MPI_Isend(&possible_root, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,&request);
+                if (thread_rank != 0)
+                    MPI_Isend(&possible_root, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,&request);
+                else
+                    solvedByThread = 0;
                 //MPI_Allreduce(&possible_root, &solvedByThread, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
                 printf("solved: after all reduce, %d\n", thread_rank);
                 break;
