a5d67d356b4cde1a708a054036cc37a031229bdd
Mitranell <milantheeuwes@live.nl>
2018-05-18 15:18:23 +0100

    hi

--- solve.c
+++ solve.c
@@ -85,7 +85,7 @@ struct cell findEmptyCell(){
 }
 
 int solve() {
-    if (solvedbythread != -1) return 0;
+    if (solvedByThread != -1) return 0;
     int rating = INT_MAX;
     int previous_rating;
 
