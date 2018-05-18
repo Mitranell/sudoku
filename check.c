dafb788d99e9a17a9c18db2d40eb47f034e6afd4
Mitranell <milantheeuwes@live.nl>
2018-05-18 15:51:33 +0100

    hi

--- check.c
+++ check.c
@@ -54,7 +54,7 @@ int checkColumn() {
 
             // if the sum equals 1, then it was the only one
             if (sum == 1) {
-                updateCell(i, p, k);
+                //updateCell(i, p, k);
             }
 
             total_sum += sum;
